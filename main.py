8c2d6eace04cfffc264438e6c547ffdc2094ab7c
Boris Kuznecov <oralebot@mail.ru>
2020-04-15 19:20:59 +0700

    popivu

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 
+t ={}
+count = {}
+types = {}
+time = 0
+with open('input.txt') as f1:
+    q = f1.readlines()
+
+for i in q:
+    time +=1
+    i = i.replace('\n','').split(' ')
+    types[time] = i
+
+
+print(types)
+
