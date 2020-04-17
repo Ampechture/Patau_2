78f98732aaed913506300c79e78ab3a4a5150601
Boris Kuznecov <oralebot@mail.ru>
2020-04-18 00:32:59 +0700

    popivu

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 
 t ={}
 count = {}
@@ -11,6 +13,22 @@ for i in q:
     i = i.replace('\n','').split(' ')
     types[time] = i
 
+def time(count_oil, income_time):
+    z = [0, 1, -1]
+    r = str(income_time).split(':')
+    hour = r[0]
+    minut = r[1]
+    total_time = round(count_oil/10)
+    minut =+ total_time + random.choice(z)
+    if minut >= 60:
+        hour +=1
+        minut -=60
+        return str(f'{hour}:{minut}')
+    else:
+        return str(f'{hour}:{minut}')
 
-print(types)
 
+
+
+
+print(time(2, '13:45'))
